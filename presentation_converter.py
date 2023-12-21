da11fcbb7895e60770c72045b0fd377f859fb008
sejalgupta <sgupta.4902@gmail.com>
2023-12-21 16:04:13 -0500

    [Presentation] Presentation created through GPT prompt

--- presentation_converter.py
+++ presentation_converter.py
@@ -2,6 +2,7 @@ from pptx import Presentation
 from constants import layout_mapping
 import json
 from helper_functions import scale_image
+from slide_content import get_metadata_gpt
 
 def add_slide(presentation, information):
     """
@@ -17,24 +18,24 @@ def add_slide(presentation, information):
     slide = presentation.slides.add_slide(slide_layout)
 
     for placeholder in slide.placeholders:
-        
+        print(placeholder.name)
         # ADD SUBTITLE
-        if 'subtitle' in placeholder.name.lower() or "text" in placeholder.name.lower():
+        if ('subtitle' in placeholder.name.lower() or "text" in placeholder.name.lower()) and len(information["subtitle"]) > 0:
             title = information["subtitle"].pop(0)
             placeholder.text = title
         
         # ADD TITLE
-        elif 'title' in placeholder.name.lower():
+        elif 'title' in placeholder.name.lower() and len(information["title"]) > 0:
             title = information["title"].pop(0)
             placeholder.text = title
 
         # ADD PICTURE / ICON
-        elif 'picture' in placeholder.name.lower():
+        elif 'picture' in placeholder.name.lower() and len(information["picture"]) > 0:
             img_path = information["picture"].pop(0)
             placeholder.insert_picture(img_path)
 
         # ADD IMAGE OR TEXT IN CONTENT HOLDER
-        elif 'content' in placeholder.name.lower():
+        elif 'content' in placeholder.name.lower() and len(information["content"]) > 0:
             content_dict = information["content"].pop(0)
             if content_dict["type"] == "text":
                 tf = placeholder.text_frame
@@ -46,22 +47,37 @@ def add_slide(presentation, information):
                 # add image as a shape
                 slide.shapes.add_picture(img_path, placeholder.left, placeholder.top, image_width, image_height)
 
-def create_presentation(name):
+def create_presentation(name, role, presentation_context, presentation_length):
     pres = Presentation()
 
     #load slides content
-    slides_content = json.load(open(f"json/{name}.json"))
+    slides_content = None
+    try:
+        slides_content = json.load(open(f"json/{name}.json"))
+    except:
+        answers = get_metadata_gpt(role, presentation_context, presentation_length)
+        response = answers["choices"][0]["message"]["content"]
+
+        slides_content = json.loads(response)
+        with open(f"json/{name}.json", 'w') as f:
+            json.dump(slides_content, f)
 
     #add each slide
-    for slide in slides_content:
-        add_slide(pres, slide)
+    for slide in slides_content["slides"]:
+        try:
+            add_slide(pres, slide)
+        except:
+            print("ERROR", slide["layout"])
 
     pres.save(f'presentation/{name}.pptx')
     print("Presentation created successfully:", name)
 
 def main():
-    for name in ["example_presentation", "multiplying_fractions"]:
-        create_presentation(name)
+    for name, role, presentation_context, presentation_length in [
+        ("multiplying_fractions", "teacher for sixth grade math", "I am trying to teach about multiplying fractions. I need to create a lesson plan for tomorrow.", "10-minute"),
+        ("dividing_fractions", "teacher for sixth grade math", "I am trying to teach about dividing fractions. I need to create a lesson plan for tomorrow.", "10-minute")
+    ]:
+        create_presentation(name, role, presentation_context, presentation_length)
 
 if __name__ == "__main__":
     main()
---
+++ slide_content.py
@@ -0,0 +1,81 @@
+import openai
+import requests
+from utils import OPENAI_API_KEY
+import json
+
+def get_prompt(role, presentation_context, presentation_length, example_presentation):
+    # PROVIDE CONTEXT
+    prompt = f"I am a {role}. {presentation_context} Can you please provide a slide by slide structure for a {presentation_length} presentation?"
+    
+    # CONSTRAINTS FOR EACH TYPE OF SLIDE
+    prompt += "Here are the types of slides and the information that they need: - TITLE_SLIDE: 1 title, 1 subtitle \n - TITLE_AND_CONTENT_SLIDE: 1 title, 1 content \n- COMPARISON: 1 title, 2 subtitles, 2 contents \n- TWO_CONTENT: 1 title, 2 contents \n- SECTION_HEADER: 1 title, 1 subtitle \n- TITLE_ONLY: 1 title \n- BLANK: 1 title \n- CONTENT_WITH_CAPTION: 1 title, 1 subtitle, 1 content \n- PICTURE_WITH_CAPTION: 1 title, 1 subtitle, 1 picture"    
+    
+    # REQUEST JSON FORMAT
+    prompt += "The slideshow should be provided in a JSON format like in the example below. Each slide should be formatted as a dictionary with "
+
+    # FORMAT OF THE INFORMATION
+    prompt += "\"layout\": REQUIRED and chosen from 'TITLE_SLIDE', \"TITLE_AND_CONTENT_SLIDE\", \"COMPARISON\", \"TWO_CONTENT\", \"SECTION_HEADER\", \"TITLE_ONLY\", \"BLANK\", \"CONTENT_WITH_CAPTION\", \"PICTURE_WITH_CAPTION\"]"
+    prompt += "\"title\": REQUIRED in list format"
+    prompt += "\"subtitle\": REQUIRED DEPENDING ON TYPE OF SLIDE in list format with each element being a different subtitle"
+    prompt += "\"picture\": REQUIRED PICTURE DEPENDING ON TYPE OF SLIDE in a list of image paths"
+    prompt += "\"content\": CONTENT DEPENDING ON TYPE OF SLIDE in a list of dictionarys format with each element being a different content. each dictionary should have the type which is image or text and the information in a \"image\" key or \"text\" key"
+    
+    # EXAMPLE PRESENTATION
+    prompt += f"Here is an example presentation to follow {example_presentation}"
+
+    return prompt
+
+def get_metadata_gpt(role, presentation_context, presentation_length):
+    example_presentation = open("json/example_presentation.json")
+    prompt = get_prompt(role, presentation_context, presentation_length, example_presentation)
+
+    answers = None
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {OPENAI_API_KEY}"
+    }
+
+    payload = {
+        "model": "gpt-4-1106-preview",
+        "response_format": {
+            "type": "json_object"
+        },
+        "messages": [
+            {
+            "role": "user",
+            "content": [
+                {
+                "type": "text",
+                "text": prompt
+                },
+            ],
+            }
+        ],
+        "max_tokens": 1000
+    }
+
+    response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+
+    # Check if the response is successful
+    if response.status_code == 200:
+        # Parse the response as JSON
+        answers = response.json()
+    else:
+        # Handle errors (non-200 responses)
+        print(f"Error: {response.status_code} - {response.text}")
+
+    return answers
+
+if __name__ == "__main__":
+    role = "teacher for sixth grade math"
+    presentation_context = "I am trying to teach about multiplying fractions. I need to create a lesson plan for tomorrow."
+    presentation_length = "10-minute"
+
+    answers = get_metadata_gpt(role, presentation_context, presentation_length)
+    response = answers["choices"][0]["message"]["content"]
+    
+    prompt = json.loads(response)
+
+    with open('data.json', 'w') as f:
+        json.dump(prompt, f)
